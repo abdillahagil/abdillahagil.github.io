d86e1c94955d825de85010cfafc9ffd31608c9ef
abdillahagil <abdillahibinagil007@gmail.com>
2022-06-20 03:07:18 +0300

    My first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+// function validateForm() {  
+//     //collect form data in JavaScript variables  
+//     var pw1 = document.getElementById("InputPassword").value;  
+    
+//     //check empty password field  
+//     if(pw1 == "") {  
+//       document.getElementById("message1").innerHTML = "**Fill the password please!";  
+//       return false;  
+//     }    
+     
+//     //minimum password length validation  
+//     if(pw1.length < 8) {  
+//       document.getElementById("message1").innerHTML = "**Password length must be atleast 8 characters";  
+//       return false;  
+//     }  
+ 
+//  }  
+
+
